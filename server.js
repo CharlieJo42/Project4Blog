b11d4afaee3362b3a1b5ed534b78979bb74edbda
Charlie Jo <charliejames1652@gmail.com>
2020-10-05 11:17:26 -0400

    show ejs added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
@@ -8,20 +9,17 @@ mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnified
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
-app.use('/articles', articleRouter)
 
 
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
 
-
-    }]
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({
+      createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
 
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
